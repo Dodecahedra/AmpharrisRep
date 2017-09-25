4ca269175d113fee21e310a75ddf505267d2336b
Koen Degeling <k.degeling@student.tue.nl>
2017-09-25 15:58:55 +0200

    Added comments for explanation

--- Ampharris/Ampharris.ino
+++ Ampharris/Ampharris.ino
@@ -4,7 +4,9 @@
 // pin number of on-board LED
 int ledPin = 13;
 
-// Pulse Width Modulation (PWM) pins
+// We initialise the pins we use for PWM, used for the
+// motors. The frequency of the PWM determines the resulting
+// voltage to the motors.
 int PWM1 = 3;
 int PWM2 = 5;
 int PWM3 = 6;
@@ -12,25 +14,30 @@ int PWM4 = 11;
 
 #include <Servo.h>  // standard servo library
 
-Servo servoX; // declare servo objects
+//Servo objects with their own function for control
+Servo servoX;
 Servo servoY;
 
-int posY = 90; // initial servo posititions in degree
+// initial servo posititions in degree
+int posY = 90;
 int posX = 90;
-
-int servoPinX = A0; // arduino pins used to connect servo control signals
+//Initialise the servo pins
+int servoPinX = A0;
 int servoPinY = A1;
 
 // setup() is only run once
 void setup() {
   Serial1.begin(115200); // ethernet cable or WiFi
 
+  //Setup servo objects
   servoX.attach(servoPinX);
   servoY.attach(servoPinY);
 
-  servoX.write(posX); // move to middle position
+  //Servo function to write the position of the servo
+  servoX.write(posX);
   servoY.write(posY);
-  // all outputs to zero
+  /* We start with all motors of, so we write 0 to all
+   * PWM pins */
   analogWrite(PWM1,0);
   analogWrite(PWM2,0);
   analogWrite(PWM3,0);
@@ -43,15 +50,18 @@ void setup() {
      Serial1.read();
 }
 
-// the commands in loop() are repeated forever
+/* Repeated code. In here we declare stuff for the control of the ROD, functions might be extended
+ * to include better controls, better control over the PWM of the motors, etc.
+ */
 void loop() {
-	if(Serial1.available() > 0) {
-		char Command = Serial1.read();
-		// if a w,z,a or d is pressed servos move with a step of
-		// 5 degrees.
-		// s moves to mid position
-		switch(Command) {
-			case 'w':
+	//TODO: Write code for the motors that will open the hatch,
+	//      and code for the motors that control the arm.
+	if(Serial1.available() > 0) { //If we have a character in our buffer, enter if-statement
+		char Command = Serial1.read(); //Read sumbitted character
+		switch(Command) { /*Here we make a switch-case, wasdz is for controlling the servo,
+		 	 	 	 	 	*the numpad keys are used for controlling the DC-motors, this should be adapted
+		 	 	 	 	 	*as our ROD will use the 2 big wheels to turn */
+			case 'w': //Use .write to write the new position of the servo w=+y,z=-y,a=+x,d=-x, s=reset
 				posY = posY + 5;
 				servoY.write(posY);
 				break;
@@ -80,8 +90,9 @@ void loop() {
 				analogWrite(PWM3,0);
 				analogWrite(PWM4,0);
 				break;
-            // in other cases switch to all combinations of forward and reverse
-            // 255 means full speed
+            /* Currently writes full speed to motors in the direction that is pressed
+             * Should be changed such that more control is allowed over speed.
+             */
 			case '8':
 				analogWrite(PWM1,255);
 				analogWrite(PWM2,0);
